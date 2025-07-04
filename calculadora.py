0cd2b6fa8f916ad7b3efa1cdd07f690491fbc7e6
Lucas Costa <lucas.cruz.campos@escola.pr.gov.br>
2025-07-04 09:40:42 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+#calculadora simples
+print ("Vamos somar dois números!")
+num1= float(input("Digite o primeiro número: "))
+num2= float(input("Digite o segundo número: "))
+soma=print ({num1 + num2})
